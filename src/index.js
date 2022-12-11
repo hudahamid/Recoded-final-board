267ac64ef2ed559bbe6db1613b377cbc2c9c26c9
Dalia Jaber <daliajaber96@gmail.com>
2022-12-11 10:43:39 +0200

    add navbar with huda's last update

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { collection, addDoc } from "firebase/firestore"; 
+import "./styles.css";
+import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
