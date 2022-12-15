87aad38e79cd38a815fe5ad632508b66ab6eec44
shad mustafa <shadmanmi2018@gmail.com>
2022-12-15 20:18:02 +0300

    doing

--- src/Component/tasks/ToDo.js
+++ src/Component/tasks/ToDo.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import db from "../../firebase";
 import { collection, addDoc, onSnapshot } from "firebase/firestore"
 import TasksCard from "./TasksCard";
+import Doing from './Doing';
 
 function ToDo(){
     //const [Title, setTitle] = useState("")
@@ -62,114 +63,109 @@ function ToDo(){
 
    
     return (
-      <Container>
+      <><Container>
         <div className='card1'>
-       
-        <h1>ToDo</h1>
 
-        <div className='CNTAINER'></div>
+          <h1>ToDo</h1>
+
+          <div className='CNTAINER'></div>
 
 
-        <form className="form-todo" style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    marginTop: "20px",   
-                }}   
-                  onSubmit={handleSubmit}   
-                    >
-         <label>Title</label>
-         <input
-            type="text"
-            placeholder="Task Title"
-            name="title"
-            value={newTaskInput.title}
-            onChange={handleOnChange}
-          />
-          <label>Describtion</label>
+          <form className="form-todo" style={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: "20px",
+          }}
+            onSubmit={handleSubmit}
+          >
+            <label>Title</label>
             <input
-            className="desc"
-            type="text"
-            placeholder="Describtion"
-            name="description"
-            value={newTaskInput.description}
-            onChange={handleOnChange}
-          />
-          
-            <label>Assign</label>
-           <input
-            type="text"
-            placeholder="Assign"
-            name="assign"
-            value={newTaskInput.assign}
-            onChange={handleOnChange}
-          />
-           <label>Started</label>
-           <input
-            type="text"
-            placeholder="start"
-            name="start"
-            value={newTaskInput.start}
-            onChange={handleOnChange}
-          />
+              type="text"
+              placeholder="Task Title"
+              name="title"
+              value={newTaskInput.title}
+              onChange={handleOnChange} />
+            <label>Describtion</label>
+            <input
+              className="desc"
+              type="text"
+              placeholder="Describtion"
+              name="description"
+              value={newTaskInput.description}
+              onChange={handleOnChange} />
 
-        <div>
-     
-            <button type="submit" className="submit-btn"  text={"Add new task"} >Add Task</button>
+            <label>Assign</label>
+            <input
+              type="text"
+              placeholder="Assign"
+              name="assign"
+              value={newTaskInput.assign}
+              onChange={handleOnChange} />
+            <label>Started</label>
+            <input
+              type="text"
+              placeholder="start"
+              name="start"
+              value={newTaskInput.start}
+              onChange={handleOnChange} />
+
+            <div>
+
+              <button type="submit" className="submit-btn" text={"Add new task"}>Add Task</button>
+            </div>
+
+
+          </form>
+
+
+
+
+
+
+
+
+          {taskList.map((task) => {
+            return (
+
+
+
+
+              <Card.Body className='card-todoo'>
+
+                <span>{task.title}</span>
+                <Link to={`/task/${task.id}`} key={task.id}>
+
+                  <Button className='edit' variant="primary">Edit</Button>
+
+                </Link>
+
+                {/* { document.querySelectorAll('.edit').addEventListener('click', event => {
+              const CONTAINER = document.querySelector(".CONTAINER");
+                CONTAINER.innerHTML = `
+                <h1>hello</h1>
+                `;
+              })
+                } */}
+
+
+              </Card.Body>
+
+
+
+
+            );
+          })}
+
+
+
+
         </div>
 
-   
-    </form>
 
 
 
 
-
-
-
-
-    {taskList.map((task) => {
-          return (
-                 
-
-
-                    
-                      <Card.Body className='card-todoo'>
-                      
-                           <span>{task.title}</span>
-                                <Link to={`/task/${task.id}`} key={task.id}>
-
-                                      <Button className='edit' variant="primary">Edit</Button>
-
-                                </Link>
-                              
-                                {/* { document.querySelectorAll('.edit').addEventListener('click', event => {
-                              const CONTAINER = document.querySelector(".CONTAINER");
-                                CONTAINER.innerHTML = `
-                                <h1>hello</h1>
-                                `;
-                              })
-                                } */}
-
-                  
-                      </Card.Body>
-                  
-
-                  
-                  
-                  );
-        })}
-
-
-        
-    
-      </div>
-
-
-
-
-
-    </Container>
-
+      </Container><Doing /></>
     
     
     
